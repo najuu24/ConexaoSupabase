92aeedc83e52507dd4a04fcd08fcc985880a7a0c
Ana <anajulia.vieira.tds24@gmail.com>
2026-09-14 17:14:58 -0300

    Supabase (BD)

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { supabase } from "./supabase";
+
+function App() {
+    async function teste() {
+        const { data, error } = await supabase.from('produtos').select()
+        console.log(data)
+    }
+
+
+    return (
+
+        <div>
+            <h1>Conexão com Supabase</h1>
+            <button onClick={teste}>Testar</button>
+        </div>
+
+    );
+
+}
+
+export default App;
